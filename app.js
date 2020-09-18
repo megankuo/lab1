6479b177e6ea0824c9742755b9235ce0f6f492f3
Megan Kuo <megankuo@Megans-MacBook-Pro.local>
2020-09-17 19:25:04 -0700

    completed lab1

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("hello");
+console.log(3+2);
+
+// Ctrl + ~ will open the TERMINAL/COMMAND PROMPT/Console in VSCode
+
+// To run a js file:
+    // “node app.js”
+
+// Shortcut for console.log()
+    // Type ‘node’ in terminal (jumps INTO node)
+    // now can type directly Into the terminal node
+    // js can be executed without console.log
+    // Type ‘.exit’ to finish
+
+var importantEq = 45 * 2;
+console.log("The important equation you need to know is: " + importantEq);
+
+// Shift + alt + a
+/* ljshglsg */
+
+// ctrl + /
+
+let num = 10;
+
+if (num % 2 == 0) {
+    console.log('This number is even.')
+} else {
+    console.log('This number is odd.')
+}
+
+function test(greeting) {
+    console.log(greeting + " My name is Megan.")
+}
+
+var myGreeting = test("Bonjour");
+console.log(myGreeting);
+
+
--- lecture1.js
+++ lecture1.js
@@ -1,5 +1,57 @@
-var favoriteMovies = [];
+// Add user suggested movie name to list of favorite movies if movie does not have the word 'The'
+const favoriteMovies = [];
 
 function addFavoriteMovie(movieName) {
-    if (movieName)
+	let movieWords = movieName.split( " " );
+	if (!movieWords.includes("The")) {
+		favoriteMovies.push(movieName);
+	}
 }
+
+// Prints the list of favorite movies and number of movies in the list
+function printFavoriteMovies() {
+	console.log("You have " + favoriteMovies.length + " favorite movies. They are: ");
+	for (i =0; i < favoriteMovies.length; i++) {
+		console.log(favoriteMovies[i]);
+	}
+}
+
+addFavoriteMovie("The Dark Knight");
+addFavoriteMovie("Riddick");
+addFavoriteMovie("The Pirates of the Caribbean");
+addFavoriteMovie("Mulan");
+printFavoriteMovies(); // Prints: You have 2 favorite movies. They are: Riddick Mulan
+
+
+// Convert fahrenheit to celcius
+function convertFtoC(fahrenheit) {
+	let conversion = (fahrenheit - 32) * (5/9);
+	let celcius = Math.round(conversion * 100) / 100;
+	return celcius;
+}
+
+// Suggest to user what to wear for the current temperature
+function whatToWear(temperature, unit) {
+	let celcius;
+	if(unit === "fahrenheit"){
+		celcius = convertFtoC(temperature); // Convert fahrenheit to celcius
+	} else if(unit === "celcius"){
+		celcius = temperature;
+	} else {
+		console.log("This is an invalid unit. Please specify celcius or fahrenheit.");
+		return; // Prints error message and ends function
+	}
+	if(celcius <= 5){
+		console.log("It is very cold right now, you need a thick sweater, a winter coat and insulated boots.");
+	} else if(celcius <= 10){
+		console.log("It is cold right now, you should wear a sweater with long pants or leggings");
+	} else if(celcius <= 18){
+		console.log("It is chilly right now, you should bring a light jacket.");
+	} else if(celcius <= 22){
+		console.log("You will probably be comfortable in a T-shirt and pants or leggings.");
+	} else {
+		console.log("Put on your summer clothes!");
+	}
+}
+
+whatToWear(32, "fahrenheit"); // Prints It is very cold right now, you need a thick sweater, a winter coat and insulated boots.
